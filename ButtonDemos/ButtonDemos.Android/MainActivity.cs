a3b120cd524736c685c3b2c46abac61dc2a6732c
Igor Simoes <krishnarupa@gmail.com>
2023-05-04 11:33:38 -0300

    Add project files.

---
+++ ButtonDemos/ButtonDemos.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Dynatrace.Xamarin;
+
+namespace ButtonDemos.Droid
+{
+    [Activity(Label = "ButtonDemos", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+            Agent.Instance.Start();
+            Agent.Instance.IdentifyUser("igor.simoes@dynatrace.com");
+        }
+    }
+}
+
